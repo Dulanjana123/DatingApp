82b5b978d75617841105e970e20524b9188fd536
Dulanjana <Dulanjana@DESKTOP-4MSIP85>
2022-09-10 22:08:20 +0530

    End of section 5

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  //create class property to store what user entered into the form
+  model: any = {}
+
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+  }  
+
+
+}
