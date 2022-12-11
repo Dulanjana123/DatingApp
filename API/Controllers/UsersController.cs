16adf87084c9a71b9768369713fe78c705ebcb36
Dulanjana <Dulanjana@DESKTOP-4MSIP85>
2022-12-11 13:26:25 +0530

    update to .Net 7

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -46,6 +47,20 @@ namespace API.Controllers
             return await _userRepository.GetMemberAsync(username); //get entity from memmory from this request
 
             //return _mapper.Map<MemberDto>(user); // in memmory we map one object to another
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username =  User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            if(user == null) return NotFound();
+
+            _mapper.Map(memberUpdateDto, user);
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+            return  BadRequest("Failed to update user");
         }
 
 
